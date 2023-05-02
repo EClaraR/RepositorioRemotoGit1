0054980d71fe4238f80bac1c0bfdc384162f27aa
usuario <usuario@DESKTOP-0VSBK2V>
2023-05-02 19:59:15 +0200

    Tercer commit

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Ejemplo control de versiones con Git");
 		System.out.println("Escribimos otra linea para hacer un segundo commit");
+		System.out.println("Nueva salida de datos");
 	}
 
 }
