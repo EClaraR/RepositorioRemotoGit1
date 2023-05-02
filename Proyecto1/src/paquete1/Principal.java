4578c2cc5beb784e7db7ec25b543e601d3fb3695
usuario <usuario@DESKTOP-0VSBK2V>
2023-05-02 18:56:31 +0200

    Segundo commit

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Ejemplo control de versiones con Git");
+		System.out.println("Escribimos otra linea para hacer un segundo commit");
 	}
 
 }
